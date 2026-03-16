22601bbc03bbc24432baa988282a04471d7e04bd
bwernest <ernest.laprelleboucher@gmail.com>
2026-03-16 23:09:54 +0100

    Notes IA

--- main_dev.py
+++ main_dev.py
@@ -3,12 +3,30 @@
 - Parsing pré game pour désactiver les attaques non activables
 - Parsing pré game pour compter le nombres de cartes de chaque type (opti attaques)
 - Contrôle : effet return sur le round, se termine juste maintenant
-- Les effets return sont DANS le tour, et ensuite décompte du tour
 - But de l'hiver : trier les cartes par interet
     -> Fonction selection qui a une carte calcule un score
 - Tests d'un play à une carte (centrosaurus)
 - On draw, le burn affecte d'un tick les cartes adverses (hellboy)
-- Check un effet affectant les 2 joueurs pour une resource per turn
+- Coder plusieurs IA qui s'affronteront :
+    - Mallia : Jouer au hasard
+    - Mauvaka : Maximiser le power au tour T
+    - Marchand : Maximiser le power à la fin du round
+    - Novès :   Maximiser le power total du deck au tour T
+    - Mola :    Maximiser le power total du deck au tour T+1
+    - Lacroix : Maximiser le power total du deck au tour T+2
+    
+    - Ramos :   Novès puis Marchand à X-2
+    - Ntamack : Mola puis Marchand à X-2
+    - Dupont :  Lacroix puis Marchand à X-2
+    - Lebel :   Novès puis Marchand à 2-X
+    - Capuozzo : Mola puis Marchand à 2-X
+    - Kinghorn :  Lacroix puis Marchand à 2-X
+    - Jelonch : Novès puis Mauvaka à X-2
+    - Willis : Mola puis Mauvaka à X-2
+    - Meafou : Lacroix puis Mauvaka à X-2
+    - Graou : Novès puis Mauvaka à 2-X
+    - Barassi : Mola puis Mauvaka à 2-X
+    - Ahki : Lacroix puis Mauvaka à 2-X
 """
 """___Modules_______________________________________________________________"""
 
