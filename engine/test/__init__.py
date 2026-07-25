7320d50915297ba0e4d08d53c0ba075f0646d859
bwernest <ernest.laprelleboucher@gmail.com>
2026-07-25 13:34:18 +0200

    Rename generated en output

--- engine/test/__init__.py
+++ engine/test/__init__.py
@@ -10,21 +10,21 @@ from ..utils.settings import Settings
 import functools
 import os
 import shutil
-from typing import Callable
+from typing import Any, Callable
 
 """___Functions_________________________________________________________________________________"""
 
 
-def void(fonction: Callable) -> any:
+def void(fonction: Callable) -> Any:
     @functools.wraps(fonction)
-    def fct(*args, **kwargs) -> any:
+    def fct(*args, **kwargs) -> Any:
         settings = Settings("test")
         paths = settings.paths
-        if os.path.isdir(paths["folder_generated"]):
-            shutil.rmtree(paths["folder_generated"])
-        os.makedirs(paths["folder_generated"])
+        if os.path.isdir(paths["folder_output"]):
+            shutil.rmtree(paths["folder_output"])
+        os.makedirs(paths["folder_output"])
         result = fonction(*args, **kwargs)
-        shutil.rmtree(paths["folder_generated"])
-        os.makedirs(paths["folder_generated"])
+        shutil.rmtree(paths["folder_output"])
+        os.makedirs(paths["folder_output"])
         return result
     return fct
